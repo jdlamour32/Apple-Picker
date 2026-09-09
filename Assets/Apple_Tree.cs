64661890fdec0032c56d6f8d7389c4093b93c9e3
jdlamour32 <jdlamour32@gmail.com>
2026-09-09 10:46:15 -0400

    Initial commit

---
+++ Assets/Apple_Tree.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Apple_Tree : MonoBehaviour
+{
+[Header("Inscribed")]
+public GameObject applePrefab;
+
+public float speed = 1f;
+
+public float leftAndRightEdge = 10f;
+
+public float changeDirChance = 0.1f;
+
+public float appleDropDelay = 1f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
